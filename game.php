416429b0d287ad66a795581e435f62a6525629d6
kr-istin <kr.reich@web.de>
2015-12-06 22:02:12 +0100

    Variablen angepasst, Parameter für combatSimulator() , Formatierung

--- game.php
+++ game.php
@@ -6,9 +6,7 @@
         $continue = true;
         $randomEvent;
         
-        $lvlHelper;
-        $lvlOpponent;
-        $opponentType;                                          // wird zur Anzeige und Bestimmung eines gegnertyps genutzt
+        $amountTypes = 0;
         $opponents = array("Ratte", "Schwein", "Kaninchen");    // Enthält alle Gegnertypen
         
         $inventar = array();
@@ -24,7 +22,8 @@
             $randomEvent = rand(0,2);       // Balancing muss noch angepasst werden!
             if($randomEvent == 1){          // bei einer 1 wird ein Kampf vor das Weitergehen geschoben
                 include 'combat.php';
-                combatSimulator();    // ruft den Kampfsimulator auf, übergibt mehrere Parameter die innerhalb der Funktion gebraucht werden       
+                $amountTypes = count($opponents);
+                combatSimulator($inventar, $opponents, $amountTypes);    // ruft den Kampfsimulator auf, übergibt mehrere Parameter die innerhalb der Funktion gebraucht werden       
             }
             else if($randomEvent == 2) {    // bei einer 2 wird vor dem Weitergehen ein Juwel aufgehoben
                 $getGem = foundGem();       // ruft die Juwelen-Funktion auf, weist Rückgabewert einer Variable zu, damit das Juwel leicht ins Array eingefügt werden kann
@@ -47,7 +46,7 @@
     
         function foundGem() {
             $newGem = rand(0, 3);   // Bonus des Juwels wird zufällig ausgewählt. Muss noch ans Balancing angepasst werden
-            echo "Du hast ein Juwel gefunden. Es hat die Stufe " . $newGem . ". Dein Helfer scheint nun stärker zu sein.<br>";
+            echo "Du hast ein Juwel gefunden. Es hat die Stufe " . $newGem . ". Dein Helfer scheint nun stärker zu sein.<br><br>";
             return $newGem;
         }
  
