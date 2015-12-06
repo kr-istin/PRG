a6e7867bad03f4d39bac7078a33e41029d07edec
kr-istin <kr.reich@web.de>
2015-12-06 16:51:24 +0100

    foundGem() Funktion hinzugefügt, Syntax angepasst, Kommentare ausgeweitet, Formatierung

--- game.php
+++ game.php
@@ -1,28 +1,48 @@
 <?php
 
-    // Variablen 
-    $continue = true;
-    $steps = 0;
-    $randomEvent;
+    /* ***********************************************
+     *      Variablen
+     *********************************************** */
+        $continue = true;
+        $randomEvent;
+        
+        $inventar = array();
+        $getGem;
     
-    // Programmablauf 
-    do {
-        $steps;
-        $randomEvent = rand(0,2);       // im Spiel mehr Zahlen fÃ¼r geringere Wahrscheinlichkeit eines Events?
-        if($randomEvent == 1){          // bei einer 1 wird ein Kampf vor das Weitergehen geschoben
-            echo "Kampfsimulator wird ausgefÃ¼hrt.<br>";     // Platzhalter Ausgabe als Test       
-        }
-        else if($randomEvent == 2) {    // bei einer 2 wird vor dem Weitergehen ein Juwel aufgehoben
-            echo "Es wird ein Juwel gefunden.<br>";      // Platzhalter Ausgabe als Test  
-        }
-        
-        /*  An dieser Stelle sollen dann der normale Text und die AuswahlmÃ¶glichkeiten ausgegeben werden
-         *  Die Ausgabe erfolgt abhÃ¤ngig von der Datenbank (Relationen innerhalb dieser)
-         */
-        echo "Der normale Spielfluss wird fortgefÃ¼hrt.<br>";     // Platzhalter Ausgabe als Test  
-        
-        $continue = false;
-        
-    } while($continue);
     
+    
+    /* ***********************************************
+     *      Programmablauf 
+     *********************************************** */
+    
+        do {
+            $randomEvent = rand(0,2);       // Balancing muss noch angepasst werden!
+            if($randomEvent == 1){          // bei einer 1 wird ein Kampf vor das Weitergehen geschoben
+                echo "Kampfsimulator wird ausgefuehrt.<br>";     // Platzhalter Ausgabe als Test       
+            }
+            else if($randomEvent == 2) {    // bei einer 2 wird vor dem Weitergehen ein Juwel aufgehoben
+                $getGem = foundGem();       // ruft die Juwelen-Funktion auf, weist Rückgabewert einer Variable zu, damit das Juwel leicht ins Array eingefügt werden kann
+                $inventar[] = $getGem;      // fügt das neue Juwel hinten ans Inventar-Array 
+            }
+
+            //  An dieser Stelle sollen dann der normale Text und die Auswahlmoeglichkeiten ausgegeben werden
+            echo "Der normale Spielfluss wird fortgefuehrt.<br>";     // Platzhalter Ausgabe als Test  
+
+            //  folgende Zeile sollte außerhalb der Tests natürlich gelöscht/auskommentiert werden
+            $continue = false;
+
+        } while($continue);
+    
+    
+    
+    /* ***********************************************
+     *      Funktionen Abschnitt
+     *********************************************** */
+    
+        function foundGem() {
+            $newGem = rand(0, 3);   // Bonus des Juwels wird zufällig ausgewählt. Muss noch ans Balancing angepasst werden
+            echo "Du hast ein Juwel gefunden. Es hat die Stufe " . $newGem . ". Dein Helfer scheint nun stärker zu sein.";
+            return $newGem;
+        }
+        
 ?>
