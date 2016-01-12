1032bb97109d9727594a9d1b80d38952c52ebd05
kr-istin <kr.reich@web.de>
2016-01-12 17:43:42 +0100

    PHP Code zum Anmeldeformular hinzugefügt

---
+++ confirm_registration.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang=de >
+    
+<head>
+    <meta charset="UTF-8">
+    <title>Registrierung</title>
+</head>
+
+<body>
+    <header>
+        <h1><img src="Grafiken\labLogo.png"  width="440" height="200" alt="logo"></h1> 
+    </header>
+    
+    
+    <?php 
+        // PHP verarbeitet die Formulardaten
+        $username = $_POST['name']; 
+        $pw = $_POST['pass'];
+        $pwRepeat = $_POST['repeat'];
+        
+        // überprüft ob das wiederholte passwort übereinstimmt
+        if($pw == $pwRepeat) {      
+            echo "<p>Hallo " . $username . "!</p><br>";    // gibt kurze Bestätigung inkl. Username aus      
+            echo "<p>Danke für deine Anmeldung. Viel Spaß beim Spielen! :)</p><br><br>";
+            echo "<p><b>Achtung:</b> Dies ist eine Testausgabe. Aktuell funktioniert das einloggen noch nicht.</p>"; 
+        }
+         else {
+            echo "Die Passwortwiederholung stimmt nicht mit dem eingegebenen Passwort überein.<br>";
+            echo "Bitte geh noch einmal zurück und versuche es erneut!<br><br>";
+            echo "<a href=registrieren.html>Zurück zur Registrierung</a>";
+            }
+    ?>        
+    
+</body>
+</html>
