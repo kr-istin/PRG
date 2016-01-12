0da579de72dd503fc4a01f00f4da77824f0d41f3
kr-istin <kr.reich@web.de>
2016-01-12 17:25:53 +0100

    PHP Code zum Login Formular hinzugefügt

---
+++ confirm_login.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang=de >
+    
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+
+<body>
+    <header>
+        <h1><img src="Grafiken\labLogo.png"  width="440" height="200" alt="logo"></h1> 
+    </header>
+    
+    
+    <?php 
+        // PHP verarbeitet die Formulardaten
+        $username = $_POST['name']; 
+        $pw = $_POST['pass'];
+    ?>        
+    
+    <!-- gibt kurze Best‰tigung inkl. Username aus --> 
+    <p>Hallo <?=$username?>!</p><br>          
+    <p>Du hast dich erfolgreich eingeloggt. Viel Spaﬂ beim Spielen! :)</p><br><br>
+    
+    <p><b>Achtung:</b> Dies ist eine Testausgabe. Aktuell funktioniert das einloggen noch nicht.</p>
+        
+</body>
+</html>
