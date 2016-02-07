13dc9dc7313121481ef60c0b99832e4f2795f963
Prg-Kraft <isabell-gewinn@web.de>
2016-02-07 20:33:53 +0100

    Einfügen von Code zur Verbingungsherstellung zum Server

--- SQL/db_erstellen.php
+++ SQL/db_erstellen.php
@@ -1,4 +1,13 @@
 <?php
+	//Hier würde die Verbindung zum Server entstehen auf der die Datenbank ist
+	try {
+	   $sql = new PDO("mysql:host=localhost;dbname=$database", $username, $password);
+	   $sql->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	} catch (PDOException $e) {
+	   print "Database Error: ".$e->getMessage();
+	}
+ 
+
 /*
 	Ausführen der Querys in den Datein Antwort.sql, Login,sql, Text.sql, T_A.sql
 	Sodass die Tabellen erstellt werden
